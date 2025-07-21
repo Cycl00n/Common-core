f967eba7743b9de9360b92d363b301ea5b1c311d
Clément N <nicolas-clement1997@hotmail.fr>
2025-07-21 12:43:57 +0000

    added a bit of stuff

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,15 +6,30 @@
 /*   By: clnicola <clnicola@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 11:38:35 by clnicola          #+#    #+#             */
-/*   Updated: 2025/07/21 11:45:36 by clnicola         ###   ########.fr       */
+/*   Updated: 2025/07/21 12:43:26 by clnicola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int push_swap(int arg_count, ...)
+{
+	va_list	arg;
+	int	a;
+	int value;
+	
+	va_start(arg, arg_count);
+	for(a = 0; a <= arg_count; a++)
+	{
+		value = va_arg(arg, int);
+		ft_printf("%d \n", a);
+		
+	va_end(arg);
+	return(0);
+}
+
 int main()
 {
-	char test[20] = "yes many";
-	ft_printf("%s",test);
+	push_swap(1,2,3,4,5);
 	return(0);
 }
