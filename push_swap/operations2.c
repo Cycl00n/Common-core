79690e400f60ba2e8350952706fd597c49f20bbd
Clément N <nicolas-clement1997@hotmail.fr>
2025-07-24 15:31:25 +0000

    added a few lines at work lol

--- push_swap/operations2.c
+++ push_swap/operations2.c
@@ -6,7 +6,7 @@
 /*   By: clnicola <clnicola@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 17:30:46 by clnicola          #+#    #+#             */
-/*   Updated: 2025/07/24 11:29:46 by clnicola         ###   ########.fr       */
+/*   Updated: 2025/07/24 15:30:27 by clnicola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void	ft_rb(t_stack **stack_b, int flag)
 	t_stack	*head;
 	t_stack	*end;
 
-	if(!stack_b || !(*stack_b)-> next)
+	if(!*stack_b || !(*stack_b)-> next)
 		return;
 	head = *stack_b;
 	end = ft_stacklast(stack_b);
@@ -46,6 +46,17 @@ void	ft_rb(t_stack **stack_b, int flag)
 
 void	ft_rr(t_stack **stack_a, t_stack **stack_b, int flag)
 {
-	t_stack	head;
-	t_stack	end;
+	t_stack	*head;
+	t_stack	*end;
+
+	if(!*stack_b || !(*stack_b)-> next || !(*stack_a)-> next || !*stack_a)
+		return;
+	head = *stack_a;
+	end = ft_stacklast(stack_a);
+	end -> next = head;
+	stack_a = head -> next;
+	head -> next = NULL;
+	
+	if(flag == 1)
+		ft_printf("rr\n");
 }
